9e179dd603bfdf8cea5d1f19076849ed1e50a5f1
Jan Wong <janwong94@gmail.com>
2017-04-12 04:49:06 +0000

    edit exercise 1 to true or false values

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -8,22 +8,22 @@ puts "----------"
 store = Store.new
 store.name = "Burnaby"
 store.annual_revenue = 300000
-store.mens_apparel = "Yes"
-store.womens_apparel = "Yes"
+store.mens_apparel = true
+store.womens_apparel = true
 store.save
 
 store = Store.new
 store.name = "Richmond"
 store.annual_revenue = 1260000
-store.mens_apparel = "No"
-store.womens_apparel = "Yes"
+store.mens_apparel = false
+store.womens_apparel = true
 store.save
 
 store = Store.new
 store.name = "Gastown"
 store.annual_revenue = 190000
-store.mens_apparel = "Yes"
-store.womens_apparel = "No"
+store.mens_apparel = true
+store.womens_apparel = false
 store.save
 
 puts Store.count
