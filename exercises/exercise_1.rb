3daab37b312f5fecc0c37ec55f252843f2765630
Jan Wong <janwong94@gmail.com>
2017-04-12 04:35:25 +0000

    create 3 stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,26 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+store = Store.new
+store.name = "Burnaby"
+store.annual_revenue = 300000
+store.mens_apparel = "Yes"
+store.womens_apparel = "Yes"
+store.save
+
+store = Store.new
+store.name = "Richmond"
+store.annual_revenue = 1260000
+store.mens_apparel = "No"
+store.womens_apparel = "Yes"
+store.save
+
+store = Store.new
+store.name = "Gastown"
+store.annual_revenue = 190000
+store.mens_apparel = "Yes"
+store.womens_apparel = "No"
+store.save
+
+puts Store.count
